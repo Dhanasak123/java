2c8a2e2c9c64679be63f73799cc73dadef1870e5
Dhanasak123 <itsthiksha@gmail.com>
2022-03-29 20:06:11 +0530

    B branch updated

--- src/org/test/Remote.java
+++ src/org/test/Remote.java
@@ -5,5 +5,7 @@ public class Remote {
 	public static void main(String[] args) {
 		System.out.println("Aviation");
 		System.out.println("Mriya");
+		System.out.println("Welcome");
+		System.out.println("ATR");
 	}
 }
